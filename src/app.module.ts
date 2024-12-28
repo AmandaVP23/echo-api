60a7fd3c00a804bb981cd5130265429cd3b9b905
Amanda Vieira <amandavp23@gmail.com>
2024-12-28 21:35:01 +0000

    Created users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mariadb',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'echo-chat',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true, // should be false in production
+        }),
+        UsersModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
