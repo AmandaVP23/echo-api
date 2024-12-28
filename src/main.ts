eebbbe64f1156bd87e24fa616b239d5f483e4e92
Amanda Vieira <amandavp23@gmail.com>
2024-12-28 21:46:00 +0000

    Added api documentation (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = 8080;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    console.log('hsedesssssy');
+
+    const config = new DocumentBuilder()
+        .setTitle('Echo Chat - API Documentation')
+        .setDescription('API Documentation')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, document);
+
     await app.listen(PORT);
-    console.log('*********************************');
-    console.log(`*                               *`);
-    console.log(`*     http://localhost:${PORT}     *`);
-    console.log(`*                               *`);
-    console.log('*********************************');
+    console.log('*****************************************************');
+    console.log(`*                                                   *`);
+    console.log(`*               http://localhost:${PORT}               *`);
+    console.log(`*               http://localhost:${PORT}/docs          *`);
+    console.log(`*                                                   *`);
+    console.log('*****************************************************');
 }
 bootstrap();
